639bbb122c7d5ff37a2cbda819191ee60f9bbf39
adubey230 <dubeya2@uci.edu>
2025-01-11 21:25:10 -0800

    happy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,37 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _plantCountUI.UpdateSeeds(5, 0);
     }
 
     private void Update()
     {
-        
+      if (Input.GetKey(KeyCode.W)) 
+      {
+        _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+      } 
+      if (Input.GetKey(KeyCode.A))
+      {
+        _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+      }
+      if (Input.GetKey(KeyCode.S))
+      {
+        _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+      }
+      if (Input.GetKey(KeyCode.D))
+      {
+        _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+      }
     }
 
     public void PlantSeed ()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_numSeeds > 0)
+            {
+                Instantiate(_plantPrefab.transform.position = _playerTransform.transform.position);
+            }
+        }
     }
 }
